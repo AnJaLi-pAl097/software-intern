cf1f0ea5c78f417327d51e0feb415c0b4612c52b
AnJaLi-pAl097 <123440153+AnJaLi-pAl097@users.noreply.github.com>
2023-01-24 15:51:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//PRINT TRIANGLE- AND ALLOW USER TO SET HEIGHT OF IT IN.
+class pyramidpattern
+{
+    static void Main(string[] args)
+    {
+        int space,length=1,row;
+        Console.WriteLine("Enter row number:");
+        row=Convert.ToInt32(Console.ReadLine());
+        space=row-1;
+
+        for(int i=1;i<=row;i++)
+        {
+            for(int r=1;r<=space;r++)
+            {
+                Console.Write(" ");
+            }
+            for(int s=1;s<=length;s++)
+            {
+                Console.Write("*");
+            }
+            space--;
+            length=length+2;
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+
+    }
+}
